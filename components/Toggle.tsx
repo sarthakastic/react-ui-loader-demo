77a43024c0f48a1b9514a63fcb03d5a65868039e
sarthakastic <iamsg26@gmail.com>
2024-01-24 01:23:42 +0530

    fix : theme toggle color

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -22,12 +22,19 @@ export function ModeToggle() {
   };
 
   return (
-    <div onClick={() => handleTheme()} className="fixed z-50 top-5 right-5 ">
+    <div
+      onClick={() => handleTheme()}
+      className="fixed z-50 bottom-5 md:top-5 right-5 font-extrabold "
+    >
       <div>
         {/* <DropdownMenuTrigger asChild> */}
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          variant="outline"
+          size="icon"
+          className="bg-primaryLight dark:bg-gray-950 hover:bg-primaryLight"
+        >
+          <Sun className="h-[1.5rem] w-[1.5rem]  bg-primaryLight rotate-0 scale-100 text-secondaryLight transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 text-secondaryLight scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
         {/* </DropdownMenuTrigger> */}
