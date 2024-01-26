de93ccd849ec96e2e8492a9f9549248ce46360b6
sarthakastic <iamsg26@gmail.com>
2024-01-26 21:59:52 +0530

    add : shimmer photos

--- components/CardUI.tsx
+++ components/CardUI.tsx
@@ -10,7 +10,7 @@ import { Button } from "./ui/button";
 import { useState } from "react";
 import { CopyBlock, tomorrowNightBright } from "react-code-blocks";
 import Image from "next/image";
-import logo from "../public/logo.png";
+import logo from "../public/defaultShimmer.png";
 import {
   Dialog,
   DialogContent,
@@ -22,9 +22,9 @@ import {
 
 const CardUI = ({ e, setShow }: any) => {
   return (
-    <Card className=" bg-[#ECE3CE] dark:bg-[#001524] shadow-2xl min-h-96 m-auto mt-20 w-5/6 md:w-1/3  xl:w-1/5">
-      <CardHeader className="m-2 shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)]">
-        <Image src={logo} alt="logo" height={200} width={400} />
+    <Card className=" bg-[#ECE3CE] dark:bg-[#001524] shadow-2xl min-h-[420px] m-auto mt-20 w-5/6 md:w-1/3  xl:w-1/5">
+      <CardHeader className="m-2 p-1 h-fit shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)]">
+        {e.image}
       </CardHeader>
       <CardContent>
         <CardTitle>{e.name}</CardTitle>
@@ -45,7 +45,6 @@ const CardUI = ({ e, setShow }: any) => {
 
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>Are you absolutely sure?</DialogTitle>
               <DialogDescription>
                 <CopyBlock
                   text={e.demoCode}
--- lib/ShimmerDemo.tsx
+++ lib/ShimmerDemo.tsx
@@ -1,4 +1,24 @@
 import { ProfileShimmer } from "react-ui-loader";
+import Image from "next/image";
+import defaultShimmer from "../public/defaultShimmer.png";
+import profilePictureCenterShimmer from "../public/profilePictureCentreShimmer.png";
+import profilePictureSizeShimmer from "../public/profilePictureSizeShimmer.png";
+import roundedCornerProfileShimmer from "../public/roundedCornerProfilePictureShimmer.png";
+import squareProfileShimmer from "../public/squareProfileShimmer.png";
+import noProfilePictureShimmer from "../public/noProfilePictureShimmer.png";
+import customCoverHeightShimmer from "../public/customCoverHeightShimmer.png";
+import customCoverWidthShimmer from "../public/customCoverWidthShimmer.png";
+import noCoverShimmer from "../public/noCoverShimmer.png";
+import noLineShimmer from "../public/noLineShimmer.png";
+import customLineHeightShimmer from "../public/customLineHeightShimmer.png";
+import customLineWidthShimmer from "../public/customLineWidthShimmer.png";
+import customLineQuantityShimmer from "../public/customLineQuantityShimmer.png";
+import noCardsShimmer from "../public/noCardsShimmer.png";
+import customCardsQuantityShimmer from "../public/customCardsQuantityShimmer.png";
+import customCardHeightShimmer from "../public/customCardHeightShimmer.png";
+import customCardWidthShimmer from "../public/customCardWidthShimmer.png";
+import customBackgroundShimmer from "../public/customBackgroundShimmer.png";
+import customColorShimmer from "../public/customColorShimmer.png";
 
 const shimmerDemo = [
   {
@@ -7,12 +27,19 @@ const shimmerDemo = [
     description:
       "Default Shimmer with white background and gray shimmer effect containing cover photo, profile picture, line and cards",
     demoCode: `<ProfileShimmer show={true} />`,
+    image: <Image src={defaultShimmer} alt="default shimmer" />,
   },
   {
     name: "Profile Picture Center Shimmer",
     code: <ProfileShimmer key="2" show={true} profilePositionCenter={true} />,
     description: "Shimmer with Profile Picture in the centre of the screen ",
     demoCode: `<ProfileShimmer show={true} profilePositionCenter={true} />`,
+    image: (
+      <Image
+        src={profilePictureCenterShimmer}
+        alt="profile picture center shimmer"
+      />
+    ),
   },
   {
     name: "Profile Picture Size Shimmer",
@@ -20,96 +47,148 @@ const shimmerDemo = [
     description:
       "Shimmer with custom size of profile picture with default shape as circle",
     demoCode: `<ProfileShimmer show={true} profileDimension="50px" />`,
+    image: (
+      <Image
+        src={profilePictureSizeShimmer}
+        alt="profile picture size shimmer"
+      />
+    ),
   },
   {
     name: "Rounded Corner Profile Shimmer",
     code: <ProfileShimmer key="4" show={true} profileShape="roundedCorner" />,
     description: "Shimmer with Rounded Corner shaped profile picture",
     demoCode: `<ProfileShimmer show={true} profileShape="roundedCorner" />`,
+    image: (
+      <Image
+        src={roundedCornerProfileShimmer}
+        alt="rounded corner profile shimmer"
+      />
+    ),
   },
   {
     name: "Square Profile Shimmer",
     code: <ProfileShimmer key="5" show={true} profileShape="square" />,
     description: "Shimmer with Square shaped profile picture",
     demoCode: `<ProfileShimmer show={true}  profileShape="square" />`,
+    image: <Image src={squareProfileShimmer} alt="square profile shimmer" />,
   },
   {
     name: "No profile Picture Shimmer",
     code: <ProfileShimmer key="6" show={true} showProfile={false} />,
     description: "Shimmer with no Profile Picture",
     demoCode: `<ProfileShimmer show={true} showProfile={false}  />`,
+    image: (
+      <Image src={noProfilePictureShimmer} alt="no profile picture shimmer" />
+    ),
   },
   {
     name: "Custom Cover Height Shimmer",
     code: <ProfileShimmer key="7" show={true} coverHeight="10vh" />,
     description: "Shimmer with custom cover height",
     demoCode: `<ProfileShimmer show={true} coverHeight="10vh" />`,
+    image: (
+      <Image src={customCoverHeightShimmer} alt="custom cover height shimmer" />
+    ),
   },
   {
     name: "Custom Cover Width Shimmer",
     code: <ProfileShimmer key="8" show={true} coverWidth="40vw" />,
     description: "Shimmer with custom cover width",
     demoCode: `<ProfileShimmer show={true} coverWidth="40vw" />`,
+    image: (
+      <Image src={customCoverWidthShimmer} alt="custom cover width shimmer" />
+    ),
   },
   {
     name: "No Cover Shimmer",
     code: <ProfileShimmer key="9" show={true} showCover={false} />,
     description: "Shimmer with no cover",
     demoCode: `<ProfileShimmer show={true} showCover={false} />`,
+    image: <Image src={noCoverShimmer} alt="no cover shimmer" />,
   },
   {
     name: "No Line Shimmer",
     code: <ProfileShimmer key="10" show={true} showLines={false} />,
     description: "Shimmer with no lines",
     demoCode: `<ProfileShimmer show={true} showLines={false} />`,
+    image: <Image src={noLineShimmer} alt="no line shimmer" />,
   },
   {
     name: "Custom Line Height Shimmer",
     code: <ProfileShimmer key="11" show={true} lineHeight={"25px"} />,
     description: "Shimmer with custom line width",
     demoCode: `<ProfileShimmer show={true} lineHeight={"25px"} />`,
+    image: (
+      <Image src={customLineHeightShimmer} alt="custom line height shimmer" />
+    ),
   },
   {
     name: "Custom Line width Shimmer",
     code: <ProfileShimmer key="12" show={true} lineWidth={"50%"} />,
     description: "Shimmer with custom line width",
     demoCode: `<ProfileShimmer show={true} lineWidth={"50%"} />`,
+    image: (
+      <Image src={customLineWidthShimmer} alt="custom line width shimmer" />
+    ),
   },
   {
     name: "Custom Lines Quantity Shimmer",
     code: <ProfileShimmer key="13" show={true} numberOfLines={25} />,
     description: "Shimmer with custom number of lines",
     demoCode: `<ProfileShimmer show={true} numberOfLines={25} />`,
+    image: (
+      <Image
+        src={customLineQuantityShimmer}
+        alt="custom line quantity shimmer"
+      />
+    ),
   },
   {
     name: "No Cards Shimmer",
     code: <ProfileShimmer key="14" show={true} showCards={false} />,
     description: "Shimmer with no cards",
     demoCode: `<ProfileShimmer show={true} showCards={false} />`,
+    image: <Image src={noCardsShimmer} alt="no cards shimmer" />,
   },
   {
     name: "Custom Cards Quantity Shimmer",
     code: <ProfileShimmer key="15" show={true} numberOfCards={25} />,
     description: "Shimmer with custom number of cards",
     demoCode: `<ProfileShimmer show={true} numberOfCards={25} />`,
+    image: (
+      <Image
+        src={customCardsQuantityShimmer}
+        alt="custom card quantity shimmer"
+      />
+    ),
   },
   {
     name: "Custom Card Height Shimmer",
     code: <ProfileShimmer key="16" show={true} cardHeight="200px" />,
     description: "Shimmer with custom card height",
     demoCode: `<ProfileShimmer show={true} cardHeight="200px" />`,
+    image: (
+      <Image src={customCardHeightShimmer} alt="custom card height shimmer" />
+    ),
   },
   {
     name: "Custom Card Width Shimmer",
     code: <ProfileShimmer key="17" show={true} cardWidth="200px" />,
     description: "Shimmer with custom card width",
     demoCode: `<ProfileShimmer show={true} cardWidth="200px" />`,
+    image: (
+      <Image src={customCardWidthShimmer} alt="custom card width shimmer" />
+    ),
   },
   {
     name: "Custom Background Shimmer",
     code: <ProfileShimmer key="18" show={true} backgroundColor="#D6CDA4" />,
     description: "Shimmer with custom background color",
     demoCode: `<ProfileShimmer show={true} backgroundColor="#D6CDA4" />`,
+    image: (
+      <Image src={customBackgroundShimmer} alt="custom background shimmer" />
+    ),
   },
   {
     name: "Custom Color Shimmer",
@@ -129,6 +208,7 @@ const shimmerDemo = [
     shimmerColor="#D6CDA4"
     backgroundColor="#D6CDA4"
   />`,
+    image: <Image src={customColorShimmer} alt="custom color shimmer" />,
   },
 ];
 
